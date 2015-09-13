d9766295e08e8b596a9b908d2f9105783685a0fa
sharpxuh <tom.xu@ceosoftcenters.com>
2015-09-13 22:08:12 +0800

    edit house page

--- routes/house.js
+++ routes/house.js
@@ -5,7 +5,14 @@ var houseservice = require('../service/houseservice');
 router.get('/rentHouse',function(req,res,next){
 
   houseservice.findByIsRent('1',function(result){
-    res.render('rentHouse',{data:result});
+    res.render('rentHouse',{data:result,rent:true});
+  })
+});
+
+router.get('/secondHouse',function(req,res,next){
+
+  houseservice.findByIsRent('0',function(result){
+    res.render('rentHouse',{data:result,rent:false});
   })
 });
 
